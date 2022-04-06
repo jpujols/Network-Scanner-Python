6c04d121964b78042a7c9ddbff2fd748bb34e391
Joaquin Pujols <54540355+jpujols@users.noreply.github.com>
2022-04-06 21:14:47 +0000

    add additional code for ARP request. Combined frames and send broadcast packets

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import scapy.all as scapy
+import sr
 
-#Discover clients on network
 def scan(ip):
-  arp_request = scapy.ARP()
-  arp_request.pdst=ip
-  print(arp_request.summary())
-  scapy.ls(scapy.ARP())
+  #Discover network clients
+  arp_request = scapy.ARP(pdst=ip)
+  arp_request.show()
+  #Create broadcast message
+  broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+  broadcast.show()
+  #Send broadcast msg to everyone
+  arp_request_broadcast = broadcast/arp_request
+  arp_request_broadcast.show()
   
-scan("192.168.1.0/24")
+
+scan("10.0.2.0/24")
