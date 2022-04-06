30f7abdce6b4f955e41cc2092a98027158742027
Joaquin Pujols <54540355+jpujols@users.noreply.github.com>
2022-04-06 21:41:46 +0000

    feed client_list with dictionary values generated from the iteration of the loop

--- main.py
+++ main.py
@@ -15,11 +15,18 @@ def scan(ip):
 
   #Formats output and add 3 tabs between IP and MAC for user-friendly output
   print("IP\t\t\tMAC Address\n-----------------------")
+
+  #Hold client IP and MAC in list
+  clients_list = []
   
   #Iterate over answered packets and parse output with IP address and MAC address
   for element in answered_list:
+    #Hold client IP and MAC in dictionary
+    client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+    clients_list.append(client_dict)
     #format output to 2 columns
     print(element[1].psrc + "\t\t" + element[1].hwsrc)
-    
+
+
 
 scan("10.0.2.0/24")
