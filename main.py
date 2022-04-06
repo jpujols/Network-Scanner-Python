c3b244465ffcd73eb3a0587831d87391a0fb97fa
Joaquin Pujols <54540355+jpujols@users.noreply.github.com>
2022-04-06 21:52:23 +0000

    added function get_arguments and optparse content

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import scapy.all as scapy
-import pprint
+import optparse
 
+'''
+USAGE: main.py --target <e.g: 10.0.2.0/24"
+'''
+
+def get_arguments():
+  parser = optparse.OptionParser()
+  parser.add_option("-t", "--target", dest="target", help="Target IP / IP Range")
+  (options, arguments) = parser.parse_args()
+  return options
 
 def scan(ip):
   #Discover network clients
@@ -18,15 +27,21 @@ def scan(ip):
 
   #Hold client IP and MAC in list
   clients_list = []
-  
   #Iterate over answered packets and parse output with IP address and MAC address
   for element in answered_list:
     #Hold client IP and MAC in dictionary
     client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
     clients_list.append(client_dict)
-    #format output to 2 columns
-    print(element[1].psrc + "\t\t" + element[1].hwsrc)
+    return clients_list
+
+def print_result(results_list):
+  print("IP\t\t\tMAC Address\n-----------------------")
+  for client in results_list:
+    print(client)
+    scan_result = scan("10.0.2.0/24")
 
 
+options = get_arguments()
+scan_result = scan(options.target)
+print_result(scan_result)
 
-scan("10.0.2.0/24")
