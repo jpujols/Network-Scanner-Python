7c68a6d19867ba06bde0a0ecdac2cc16d691e8ab
Joaquin Pujols <54540355+jpujols@users.noreply.github.com>
2022-04-06 21:19:10 +0000

    Store response value (packet answer and unanswered). Timeout = no response = terminate execution

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import scapy.all as scapy
-import sr
+
 
 def scan(ip):
   #Discover network clients
   arp_request = scapy.ARP(pdst=ip)
-  arp_request.show()
   #Create broadcast message
   broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-  broadcast.show()
   #Send broadcast msg to everyone
   arp_request_broadcast = broadcast/arp_request
-  arp_request_broadcast.show()
-  
+  #Use sr (Send and Receive packets to test scanner)
+  scapy.srp(arp_request_broadcast)
+  #Store response value (packet answer and unanswered). Timeout = no response = terminate execution
+  answered, unanswered = scapy.srp(arp_request_broadcast, timeout=1)
+  print(answered)
 
 scan("10.0.2.0/24")
